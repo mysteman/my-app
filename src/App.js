da297b9623305864a078c1994076660711393e42
unknown <vishnusivasankar13@gmail.com>
2024-01-02 18:41:46 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SampleComponent from "./Components/SampleComponent";
+
+function App() {
+  return (
+    <div>
+      <SampleComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SampleComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SampleComponent = () =>{
+    return (
+        <div className="sample">
+            SmapleComponent
+        </div>
+    );
+};
+
+export default SampleComponent;
